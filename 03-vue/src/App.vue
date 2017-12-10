d41cbb81bf02df9eca26c296edf3e3fca810d62d
Jamis Charles <jacharles@paypal.com>
2017-12-09 22:23:59 -0800

    Added vue.js

---
+++ 03-vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Tic Tac Toe</h1>
+    <GameCanvas/>
+  </div>
+</template>
+
+<script>
+import GameCanvas from './components/GameCanvas'
+
+export default {
+  name: 'app',
+  components: {
+    GameCanvas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+
+
+body {
+  padding: 50px;
+  /* font: 14px "Lucida Grande", Helvetica, Arial, sans-serif; */
+}
+
+</style>
---
+++ 03-vue/src/components/GameCanvas.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <button class="clearButton" v-on:click="handleRestart">Clear</button>
+
+    <div class="fieldContainer">
+      <template v-for="(field, index) in fields">
+        <template v-if="field === false">
+          <div v-on:click="handlePlay(index)" class="field empty">&nbsp;</div>
+        </template>
+        <template v-else>
+          <div class="field">{{field.toUpperCase()}}</div>
+        </template>
+      </template>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameCanvas',
+  data () {
+    return {
+      msg: 'Tic Tac Toe',
+      fields: Array(9).fill(false),
+      player1: "x",
+      player2: "o",
+      currentPlayer: 1
+    }
+  },
+  methods: {
+    handlePlay: function(index) {
+
+      // clone fields so vue knows when update occurs
+      var fields = this.fields.slice();
+      var piece = this['player' + this.currentPlayer]; //x or o
+
+      fields[index] = piece;
+
+      // fill in the current piece, and switch player
+      this.fields = fields;
+      this.currentPlayer = this.currentPlayer === 1 ? 2 : 1; // toggle between player  1 and 2
+    },
+    handleRestart: function(event) {
+      this.fields = Array(9).fill(false); // start with array of 9 with false filled in
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+/* Tic Tac Toe board */
+.fieldContainer {
+  width: 312px;
+}
+
+.field {
+  /* background-color: red; */
+  border: 1px #eee solid;
+  display: inline-block;
+  font-size: 72px;
+  line-height: 98px;
+  text-align: center;
+  height: 100px;
+  width: 100px;
+}
+
+.empty:hover {
+  background-color: #fbfbfb;
+}
+
+.clearButton{
+  margin-bottom: 15px;
+}
+</style>
